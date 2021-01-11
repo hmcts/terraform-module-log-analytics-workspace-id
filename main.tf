665ca074b321b30607c24958903f690f16af79fc
Matt Slater <matt.slater@hmcts.net>
2021-01-11 09:44:15 +0000

    DTSPO-75 - removing ldata and qa

--- main.tf
+++ main.tf
@@ -23,14 +23,6 @@ locals {
       subscription_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
       name            = "hmcts-nonprod"
     }
-    qa = {
-      subscription_id = "7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c"
-      name            = "hmcts-qa"
-    }
-    ldata = {
-      subscription_id = "4da0ce99-35c5-491f-8a0b-56c39f7278fa"
-      name            = "hmcts-ethosldata"
-    }
     prod = {
       subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
       name            = "hmcts-prod"
